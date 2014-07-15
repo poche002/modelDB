fad97abde5896d77f2295272eec92a12dd1f0fd4
poche002 <poche_002@hotmail.com>
2014-07-15 14:41:34 -0300

    se agregan los datos de los badges en otra clase

--- model.py
+++ model.py
@@ -11,15 +11,26 @@ user_badges = Table('user_badges', Base.metadata,
      Column('badge_id', Integer, ForeignKey('badges.id'))
     )
 
+class Datosbadge(Base):
+    __tablename__ = 'datosbadges'
+
+    id = Column(Integer, primary_key=True)
+    key_name = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    badge_id = Column(Integer, ForeignKey('badges.id'))
+    cant_act = Column(Integer)
+
+    def __repr__(self):
+        return "<Badge(amount_existing'%s')>" % (self.cant_act)
+
 class Badge(Base):
     __tablename__ = 'badges'
 
     id = Column(Integer, primary_key=True)
+    key_name = Column(String)
     num_needed = Column(Integer)
     description = Column(String)
-    ####user_id = Column(Integer, ForeignKey('users.id'))
-
-    ####user = relationship("User", backref=backref('badges', order_by=id))
+    datosbadges = relationship("Datosbadge")
 
     def __repr__(self):
         return "<Badge(amount_needed'%s')>" % (self.num_needed)
@@ -28,24 +39,52 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    ####user_id = Column(Integer, ForeignKey('users.id'))
-    name = Column(String)
+    user_id = Column(String)
     fullname = Column(String)
     password = Column(String)
 
     # many to many users<->badges
     badges = relationship('Badge', secondary=user_badges, backref='users')
-
+    datosbadges = relationship("Datosbadge")
 
     def __repr__(self):
-        return "<User(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
+        return "<User(user_id='%s', fullname='%s', password='%s')>" % (self.user_id, self.fullname, self.password)
 
-user1 = User(name='User1', fullname='Ed Jones1', password='pass1')
-user2 = User(name='User2', fullname='Ed Jones2', password='pass2')
+user1 = User(user_id='User1', fullname='Ed Jones1', password='pass1')
+user2 = User(user_id='User2', fullname='Ed Jones2', password='pass2')
 badge1 = Badge(num_needed = 10, description = 'este es el badge1')
 badge2 = Badge(num_needed = 15, description = 'este es el badge2')
 badge3 = Badge(num_needed = 20, description = 'este es el badge3')
 
 
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
+def init_model():
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+def commit():
+    session = init_model()
+    session.commit()
+    session.close()
+
+def start():
+    pass
+
+def start_read_only():
+    pass
+
+def rollback():
+    session = init_model()
+    session.rollback()
+    session.close()
+
+def clear():
+    session = init_model()
+    #session.remove()
+    session.close()
+
+def get_user(user_id):
+    session = init_model()
+    user = session.query(User).filter_by(user_id=user_id).first()
+    session.close()
+    return user
